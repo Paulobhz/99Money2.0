d378d4729cccde494da79070b1e71faf78e60f06
Paulobhz <paulobhz7@gmail.com>
2020-07-22 11:02:46 -0300

    Aula 08
    feito até 48 minutos

--- UnitPrincipal.pas
+++ UnitPrincipal.pas
@@ -35,9 +35,13 @@ type
     Layout7: TLayout;
     Label8: TLabel;
     Label9: TLabel;
-    ListView1: TListView;
+    lv_lancamento: TListView;
+    img_categoria: TImage;
+    procedure FormShow(Sender: TObject);
   private
-    { Private declarations }
+    procedure AddLancamento(id_lancamento, descricao, categoria: string;
+                                      valor : double; dt_lanc : TDateTime;
+                                      foto: TStream);
   public
     { Public declarations }
   end;
@@ -48,5 +52,45 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TFrmPrincipal.AddLancamento(id_lancamento, descricao, categoria: string;
+                                      valor : double; dt_lanc : TDateTime;
+                                      foto: TStream);
+var
+    bmp : TBitmap;
+begin
+    with lv_lancamento.Items.Add do
+    begin
+        TListItemText(Objects.FindDrawable('TxtDescricao')).Text := descricao;
+        TListItemText(Objects.FindDrawable('TxtCategoria')).Text := categoria;
+        TListItemText(Objects.FindDrawable('TxtValor')).Text     := FormatFloat('#,##0.00',valor);
+        TListItemText(Objects.FindDrawable('TxtData')).Text      := FormatDateTime('dd/mm',dt_lanc);
+
+        if foto <> nil then
+        begin
+            bmp := TBitmap.Create;
+            bmp.LoadFromStream(foto);
+            TListItemImage(Objects.FindDrawable('ImgIcone')).OwnsBitmap := true;
+            TListItemImage(Objects.FindDrawable('ImgIcone')).Bitmap     := bmp;
+            //não precisa destruir pq está dentro da listview;
+        end;
+
+    end;
+end;
+
+procedure TFrmPrincipal.FormShow(Sender: TObject);
+var
+    foto : TStream;
+    x : integer;
+begin
+    foto := TMemoryStream.Create;
+    img_categoria.Bitmap.SaveToStream(foto);
+    foto.Position := 0;
+
+    for x := 1 to 10 do
+        AddLancamento('00001', 'Compra de Passagem teste 123456 aaaaa bbbbb cccccc ddddddddd', 'Transporte', -45, date, foto);
+
+    foto.DisposeOf;
+end;
 
 end.
